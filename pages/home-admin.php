5fabc3bdc9a9b992d2e1e7ba8ce3905131a78a1c
machiCHUCHU <ryanmarksagaysay02@gmail.com>
2024-04-25 19:14:58 +0800

    remove bypass error for login

--- pages/home-admin.php
+++ pages/home-admin.php
@@ -15,6 +15,12 @@ if(!isset($_SESSION["id"])){
 	exit();
 }
 
+if($_SESSION['accountType'] != 'admin'){
+    session_destroy();
+    header("location: ../pages/loginPage.php");
+    exit();
+}
+
 if(isset($_REQUEST["logout"])){
 	session_destroy();
 	header("location: ../pages/loginPage.php");
