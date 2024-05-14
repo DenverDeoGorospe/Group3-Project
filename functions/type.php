58a14440cdb8fa8c9724f31bfa6ec620cb66ba8e
machiCHUCHU <ryanmarksagaysay02@gmail.com>
2024-05-14 23:35:20 +0800

    updated filter

--- functions/type.php
+++ functions/type.php
@@ -6,20 +6,6 @@ $fromdate = $_POST['from_date'] ?? null;
 $todate = $_POST['to_date'] ?? null;
 $searchVal = $_POST['forSearch'] ?? null;
 $catego = $_POST['category'] ?? null;
-if($searchVal == null && $fromdate == null && $todate == null && $catego == null){
-    $stmt = $conn->prepare("SELECT * FROM tblcapstone WHERE is_status = '1'");
-}else if($fromdate == null && $todate == null && $catego == null){
-    $stmt = $conn->prepare("SELECT * FROM tblcapstone WHERE title LIKE '%$searchVal%' AND is_status = '1'");
-}else if($fromdate == null && $todate == null && $searchVal == null && $catego != null){
-    $stmt = $conn->prepare("SELECT * FROM tblcapstone WHERE category = '$catego' AND is_status = '1'");
-}else if($fromdate != null && $todate != null && $searchVal == null && $catego == null){
-    $stmt = $conn->prepare("SELECT * FROM tblcapstone WHERE date_published BETWEEN '$fromdate' AND '$todate' AND is_status = '1'");
-}
-else if($searchVal != null && $fromdate != null && $todate != null && $catego != null){
-    $stmt = $conn->prepare("SELECT * FROM tblcapstone WHERE date_published BETWEEN '$fromdate' AND '$todate' AND title LIKE '%$searchVal%' AND category = '$catego' AND is_status = '1'");
-}else{
-    $stmt = $conn->prepare("SELECT * FROM tblcapstone WHERE date_published BETWEEN '$fromdate' AND '$todate' AND category = '$catego' AND is_status = '1'");
-}
 
 if($catego == 'Web-Application'){
     $selected = 'Web';
@@ -28,6 +14,26 @@ if($catego == 'Web-Application'){
 }else{
     $selected = 'Select';
 }
+
+if($searchVal == null && $fromdate == null && $todate == null && $catego == null){
+    $stmt = $conn->prepare("SELECT * FROM tblcapstone WHERE is_status = '1'");
+}else if($fromdate == null && $todate == null && $catego == null){
+    $stmt = $conn->prepare("SELECT * FROM tblcapstone WHERE title LIKE '%$searchVal%' AND is_status = '1'");
+}else if($fromdate == null && $todate == null && $searchVal == null && $catego != null){
+   
+    $stmt = $conn->prepare("SELECT * FROM tblcapstone WHERE category = '$catego' AND is_status = '1'");
+}else if($fromdate != null && $todate != null && $searchVal == null && $catego == null){
+    $stmt = $conn->prepare("SELECT * FROM tblcapstone WHERE date_published BETWEEN '$fromdate' AND '$todate' AND is_status = '1'");
+}
+else if($searchVal != null && $fromdate != null && $todate != null && $catego != null){
+    
+    $stmt = $conn->prepare("SELECT * FROM tblcapstone WHERE date_published BETWEEN '$fromdate' AND '$todate' AND title LIKE '%$searchVal%' AND category = '$catego' AND is_status = '1'");
+}else{
+    
+    $stmt = $conn->prepare("SELECT * FROM tblcapstone WHERE date_published BETWEEN '$fromdate' AND '$todate' AND category = '$catego' AND is_status = '1'");
+}
+
+
 
 $stmt->execute();
 $searchCapstone = $stmt->fetchAll(PDO::FETCH_ASSOC);
--- pages/home-user.php
+++ pages/home-user.php
@@ -120,7 +120,7 @@ if(isset($_REQUEST["logout"])){
                             <div class="form-group d-flex p-1">
                                 <label for="category" class="p-1">Category</label>
                                 <select class="form-control shadow-none" name="category" id="category">
-                                        <option hidden><?php echo $selected ?></option>
+                                        <option hidden>Select</option>
                                         <option value="Web-Application">Web</option>
                                         <option value="Mobile-Application">Mobile</option>
                                     </select>
